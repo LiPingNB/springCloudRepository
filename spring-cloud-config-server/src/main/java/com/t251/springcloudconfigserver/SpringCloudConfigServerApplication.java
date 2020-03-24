dcfdd70147074de11eaf5f3db86af2ec4e4ed623
lipingNBClass <1309795179@qq.com>
2020-03-24 13:48:21 +0800

    更新

--- spring-cloud-config-server/src/main/java/com/t251/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/t251/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -9,7 +9,5 @@ public class SpringCloudConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudConfigServerApplication.class, args);
     }
-        public void hhh(){
-        
-        }
+    
 }
